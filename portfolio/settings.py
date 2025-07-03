b39c3aaecada13bff9beccb8e72d648bb689e49b
Leomb <leombernardoni@gmail.com>
2025-07-03 18:31:48 -0300

    Prepare project for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8z=6vls@#*8k%nf006zt8ly&_%n_lyt0!4+wxfo_@%zn+t^$yq'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-8z=6vls@#*8k%nf006zt8ly&_%n_lyt0!4+wxfo_@%zn+t^$yq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['your-app.onrender.com']
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
